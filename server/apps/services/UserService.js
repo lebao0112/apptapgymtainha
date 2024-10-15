1f5d566cced571c12841932cb1692876599fcefb
Le Duong Chi Bao <leduongchibao0112@gmail.com>
2024-10-15 09:13:47 +0700

    gộp mobile và server thành một repo

---
+++ server/apps/services/UserService.js
@@ -0,0 +1,43 @@
+const { ObjectId } = require("mongodb");
+var config = require("./../config/setting.json");
+
+class UserService {
+  databaseConnection = require("./../database/database");
+  client;
+  userDatabase;
+  userCollection;
+
+  constructor() {
+    this.client = this.databaseConnection.getMongoClient();
+    this.userDatabase = this.client.db(config.mongodb.database);
+    this.userCollection = this.userDatabase.collection("Users");
+  }
+
+  async insertUser(user) {
+    return await this.userCollection.insertOne(user);
+  }
+
+  async getUser(id) {
+    return await this.userCollection.findOne({ _id: new ObjectId(id) });
+  }
+
+  async updateUser(user) {
+    return await this.userCollection.updateOne(
+      { _id: new ObjectId(user._id) },
+      { $set: user }
+    );
+  }
+
+  async deleteUser(id) {
+    return await this.userCollection.deleteOne({ _id: new ObjectId(id) });
+  }
+
+  async getUserList() {
+    const cursor = await this.userCollection.find().skip(0).limit(100);
+    return await cursor.toArray();
+  }
+  async getUserByEmail(email) {
+    return await this.userCollection.findOne({ Email: email });
+  }
+}
+module.exports = UserService;
