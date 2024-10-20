ab88c84af7ae321ea9e8eb4d4066704ad3bc669a
Le Duong Chi Bao <leduongchibao0112@gmail.com>
2024-10-20 15:22:48 +0700

    Thêm tính năng chỉnh sửa username

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:doan_tapgymtainha/screen/home_screen.dart';
 import 'package:doan_tapgymtainha/screen/splash_screen.dart';
 import 'package:doan_tapgymtainha/screen/trainingprogram_screen.dart';
@@ -7,6 +6,7 @@ import 'package:doan_tapgymtainha/screen/dashboard_screen.dart';
 import 'package:doan_tapgymtainha/screen/authentication/register_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
--- mobile/lib/screen/profile_screen.dart
+++ mobile/lib/screen/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:doan_tapgymtainha/service/api_service.dart'; // Import service của bạn
+import 'package:image_picker/image_picker.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -7,14 +8,16 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-
   late Future<Map<String, dynamic>> _userProfile;
+  final _nameController = TextEditingController();
+  final picker = ImagePicker();
   final double profileHeight = 144;
 
   @override
   void initState() {
     super.initState();
-    _userProfile = ApiService.fetchUserProfile(); // Gọi API để lấy thông tin người dùng
+    _userProfile =
+        ApiService.fetchUserProfile(); // Gọi API để lấy thông tin người dùng
   }
 
   @override
@@ -40,7 +43,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
             final String email = profile['Email'];
             final int height = profile['Height'];
             final int weight = profile['Weight'];
-
+            final String dateOfBirth = profile['DateOfBirth'];
+            final String gender = profile['Gender'];
+            final String? avatarUrl = profile['AvatarUrl'];
             return Container(
               color: Colors.black,
               width: double.infinity,
@@ -48,33 +53,55 @@ class _ProfileScreenState extends State<ProfileScreen> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  // Avatar và tên người dùng
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: IconButton(
+                        onPressed: () {
+                          _showEditOptions(context, name);
+                        },
+                        icon: Icon(Icons.edit, color: Colors.grey)),
+                  ),
                   CircleAvatar(
                     radius: profileHeight / 2,
-                    backgroundColor: Colors.grey.shade800, // Background color cho avatar
-                    backgroundImage: NetworkImage(
-                      'https://minimomonimi.com/cdn/shop/products/simple-avatar-minimomonimi-cartoonish-minimalist-01.png?v=1664979841', // Avatar của người dùng từ URL
-                    ),
+                    backgroundColor: Colors.grey.shade800,
+                    backgroundImage: avatarUrl == null
+                        ? AssetImage("assets/default_avatar.png")
+                        : NetworkImage(avatarUrl!) as ImageProvider,
                   ),
-                  const SizedBox(height: 8), // Khoảng cách giữa avatar và tên
-                  Text(
-                    name, // Tên người dùng
-                    style: TextStyle(
-                      color: Colors.white, // Màu chữ
-                      fontSize: 18, // Kích thước chữ
-                      fontWeight: FontWeight.bold,
-                    ),
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        gender == 'male'
+                            ? Icons.male // Icon cho nam
+                            : Icons.female, // Icon cho nữ
+                        color: gender == 'male'
+                            ? Colors.blue
+                            : Colors.pink, // Màu sắc theo giới tính
+                        size: 24, // Kích thước icon
+                      ),
+                      Text(
+                        name, // Tên người dùng
+                        style: TextStyle(
+                          color: Colors.white, // Màu chữ
+                          fontSize: 18, // Kích thước chữ
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 40), // Khoảng cách giữa avatar và thông tin người dùng
-
-                  // Thông tin Tuổi, Chiều cao, Cân nặng
+                  const SizedBox(height: 40),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 16.0),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Expanded(
-                          child: buildInfoBox('Tuổi', '28'),
+                          child: buildInfoBox(
+                              'Tuổi',
+                              calculateAge(DateTime.parse(dateOfBirth))
+                                  .toString()),
                         ),
                         Expanded(
                           child: buildInfoBox('Chiều cao', '$height cm'),
@@ -94,21 +121,124 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
+  int calculateAge(DateTime dateOfBirth) {
+    DateTime today = DateTime.now();
+    int age = today.year - dateOfBirth.year;
+
+    if (today.month < dateOfBirth.month ||
+        (today.month == dateOfBirth.month && today.day < dateOfBirth.day)) {
+      age--;
+    }
+
+    return age;
+  }
+
+  void _showEditOptions(BuildContext context, String currentName) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: Icon(Icons.image),
+                title: Text('Chỉnh sửa Avatar'),
+                onTap: () {
+                  // _editAvatarUrl(context, currenAvatarUrl);
+                }, // Thay đổi avatar
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Chỉnh sửa Tên'),
+                onTap: () {
+                  _editName(context, currentName); // Thay đổi tên
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.close),
+                title: Text('Đóng'),
+                onTap: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _editName(BuildContext context, String currentName) {
+    _nameController.text = currentName; // Hiển thị tên hiện tại
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Chỉnh sửa tên'),
+          content: TextField(
+            controller: _nameController,
+            decoration: InputDecoration(hintText: 'Nhập tên mới'),
+          ),
+          actions: [
+            TextButton(
+              child: Text('Hủy'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            TextButton(
+              child: Text('Lưu'),
+              onPressed: () {
+                // Gọi API để lưu tên mới
+                ApiService.updateUserName(_nameController.text).then((value) {
+                  // Hiển thị snackbar thông báo thành công
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Cập nhật tên thành công!'),
+                      backgroundColor: Colors.green,
+                    ),
+                  );
+
+                  // Đóng modal chỉnh sửa hiện tại và cả modal showEditOptions
+                  Navigator.of(context).pop(); // Đóng Dialog hiện tại
+                  Navigator.of(context).pop(); // Đóng Modal showEditOptions
+
+                  // Cập nhật lại thông tin người dùng trên màn hình
+                  setState(() {
+                    _userProfile = ApiService
+                        .fetchUserProfile(); // Tải lại thông tin người dùng
+                  });
+                }).catchError((error) {
+                  // Hiển thị thông báo lỗi nếu cập nhật thất bại
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Cập nhật thất bại: $error'),
+                      backgroundColor: Colors.red,
+                    ),
+                  );
+                });
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Future<void> _editAvatar() async {
+  //   final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+  //   if (pickedFile != null) {
+  //     // Gọi API để tải ảnh lên Cloudinary và cập nhật avatarUrl
+  //     final uploadedUrl =
+  //         await ApiService.uploadAvatarToCloudinary(pickedFile.path);
+  //     setState(() {
+  //       avatarUrl = uploadedUrl; // Cập nhật ảnh đại diện
+  //     });
+  //     // Cập nhật avatarUrl vào database
+  //     await ApiService.updateAvatarUrl(uploadedUrl);
+  //   }
+  // }
+
   Widget buildInfoBox(String label, String value) {
     return Container(
-      // decoration: BoxDecoration(
-      //   color: Colors.white,
-      //   borderRadius: BorderRadius.circular(8), // Rounded corners
-      //   boxShadow: [
-      //     BoxShadow(
-      //       color: Colors.black.withOpacity(0.2),
-      //       blurRadius: 5,
-      //       offset: Offset(0, 3),
-      //     ),
-      //   ],
-      // ),
-      // padding: EdgeInsets.all(16),
-      // margin: EdgeInsets.all(8),
       child: Column(
         children: [
           Text(
